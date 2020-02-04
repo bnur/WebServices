cddfba2e6cf9149ce165d15d98c18796c2343a7a
bnur <bnurdursun@gmail.com>
2020-02-04 23:22:06 +0300

    add tckn confirmation service

---
+++ Services/Controllers/TcConfirmationController.cs
@@ -0,0 +1,47 @@
+﻿using Services.Helper;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Services.Controllers
+{
+    public class TcConfirmationController : ApiController
+    {
+        LogFile logFile = new LogFile("ServiceLogs");
+        // GET: api/TcConfirmation
+        public string Get()
+        {
+            return "Running..";
+        }
+
+        // POST: api/TcConfirmation
+        public bool Post([FromBody]TC value)
+        {
+            bool result = false;
+            try
+            {
+                if (value.TCKN != null && value.FirstName != null && value.LastName != null && value.BirthYear != null)
+                {
+                    TcConfirmationService.KPSPublicSoapClient confirmation = new TcConfirmationService.KPSPublicSoapClient();
+                    result = confirmation.TCKimlikNoDogrula(value.TCKN, value.FirstName.Trim().ToUpper(), value.LastName.Trim().ToUpper(), value.BirthYear);
+                    //logFile.Message = "Başarılı";
+                }
+                else
+                {
+                    //logFile.Message="Hata! Bilgilerden bir veya daha fazlası eksik.";
+                }
+            }
+            catch (Exception e)
+            {
+                //logFile.Message = e.Message;
+                throw;
+            }
+            return result;
+        }
+        
+    }
+}
---
+++ Services/Helper/LogFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Services.Helper
+{
+    public class LogFile
+    {
+        public LogFile(string pathName)
+        {
+            PathName = pathName;
+            Date = DateTime.Now;
+        }
+        private string _message;
+        public string Message { get { return _message; } set { _message = value; ErrorLog(_message); } }
+        private DateTime Date { get; set; }
+        public string PathName { get; set; }
+
+        public void ErrorLog(string message)
+        {
+            StreamWriter sw = new StreamWriter("../../"+ PathName,true);
+            sw.WriteLine(Date.ToString()+": "+Message);
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
